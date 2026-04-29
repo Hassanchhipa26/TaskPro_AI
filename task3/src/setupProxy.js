10c817bbe29c92afab094940e59ac6afa9f0e2e7
Hassan chhipa <forhassan43@gmail.com>
2026-04-29 10:57:45 +0530

    Update API URL to Render deployment

--- task3/src/setupProxy.js
+++ task3/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://taskpro-ai-8302.onrender.com',
       changeOrigin: true,
     })
   );
